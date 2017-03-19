e13bab24be1733ba65ddfa1fb36b9c5a18a80fd2
Ghaith AlShebil <ghaiththebest55@gmail.com>
2017-03-19 07:08:03 +0200

    Update fibonacci sequence.py

--- fibonacci sequence.py
+++ fibonacci sequence.py
@@ -1,11 +1,21 @@
-size = int(input("Enter size: "))
+import random
+
+size_ = int(input("Enter size: "))
 
 
 def fibonacci(size):
     fib_ls = []
     for number in range(0, size + 1):
         fib = (((1 + 5 ** 0.5)/2) ** number - ((1 - 5 ** 0.5)/2) ** number)/5 ** 0.5
-        fib_ls.append(int(fib))
+        fib_ls.append(fib)
     print(fib_ls)
 
-fibonacci(size)
+fibonacci(size_)
+
+
+def rand_fibonacci(number):
+    fib = (((1 + 5 ** 0.5)/2) ** number - ((1 - 5 ** 0.5)/2) ** number)/5 ** 0.5
+    print(fib)
+
+r_number = random.randrange(1000)
+rand_fibonacci(r_number)
